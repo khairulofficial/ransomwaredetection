d4c2d33e5b3c6d7637f87891f11deeff452e9802
Khairul <59458479+khairulofficial@users.noreply.github.com>
2024-05-05 18:17:00 +0800

    added card to app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_card import card
 import pandas as pd
 from test import test_model
 
@@ -17,6 +18,7 @@ def main():
     st.text("")
     st.text("")
     st.text("")
+    card(title="Hello World!", text="Some description", image="http://placekitten.com/200/300", url="https://github.com/gamcoh/st-card")
 
     # instructions
     st.markdown("""
