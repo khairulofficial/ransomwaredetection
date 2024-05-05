0b715915712fc3b3cc72cd5dc444f7a70059306c
Khairul <59458479+khairulofficial@users.noreply.github.com>
2024-05-05 16:21:29 +0800

    Update spacing details in app.py

--- src/app.py
+++ src/app.py
@@ -43,6 +43,8 @@ def main():
     # upload file
     st.markdown("""
 
+
+
     
     """)
     test_file = st.sidebar.file_uploader(label="Upload Generated CSV File", type="csv", accept_multiple_files=False)
