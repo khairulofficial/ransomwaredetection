0123120556a3ff34b96417190276c52725510cac
Khairul <59458479+khairulofficial@users.noreply.github.com>
2024-05-13 09:30:23 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,18 +5,6 @@ import hydralit_components as hc
 from streamlit_option_menu import option_menu
 import matplotlib.pyplot as plt
 
-"""--------------------------------------------- In development ------------------------------------------------------------
-import pickle
-from PyPDF2 import PdfReader
-from streamlit_extras.add_vertical_space import add_vertical_space
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import get_openai_callback
-import os
-"""
 
 def plot_accuracy():
     # accuracy scores 
@@ -203,58 +191,7 @@ def show_vis_page():
 
 def show_about_page():
     st.header("Ask about the project...Feature Coming Soon!")
-"""--------------------------------------------- In development ------------------------------------------------------------
- 
-    # upload a PDF file
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
- 
-    # st.write(pdf)
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
- 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
-        # st.write(chunks)
- 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-           
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
- 
-       
- 
-        # Accept user questions/query
-        query = st.text_input("Ask questions about your PDF file:")
-        # st.write(query)
- 
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
- 
-            llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
-"""
-        
+
     
 def main():
 
